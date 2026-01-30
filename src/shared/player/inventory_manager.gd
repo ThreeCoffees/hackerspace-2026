167a0538e3c3a781edb32d59d7100680616e9071
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-01-31 00:22:08 +0100

    add inventory and picking up items from shelves

--- src/shared/player/inventory_manager.gd
+++ src/shared/player/inventory_manager.gd
@@ -1,7 +1,10 @@
 extends Node
 
+var inventory: Dictionary[Shelf.Item, int] = {}
+
 func _ready() -> void:
 	ShoppingManager.interacted_with_shelf.connect(_on_interacted_with_shelf)
 
 func _on_interacted_with_shelf(shelf: Shelf) -> void:
-	print("interacted with %s" % [shelf])
+	inventory[shelf.item] = inventory.get(shelf.item, 0) + 1
+	print(inventory)

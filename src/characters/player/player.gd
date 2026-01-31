5262158cb8e292a88b65ba8f1236de02bf941282
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-02-01 00:45:20 +0100

    add game over detection fixes

--- src/characters/player/player.gd
+++ src/characters/player/player.gd
@@ -1,8 +1,23 @@
 extends CharacterBody3D
 
 @export var is_mask_on: bool = true
+@export var infection_timer: Timer
+var is_being_infected: bool = false
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("toggle_mask"):
 		is_mask_on = not is_mask_on
 		print("is mask on:", is_mask_on)
+
+func _physics_process(delta: float) -> void:
+	if is_being_infected == true and infection_timer.is_stopped():
+		infection_timer.start()
+	if is_being_infected == false and not infection_timer.is_stopped():
+		infection_timer.stop()
+
+	is_being_infected = false
+		
+
+func _on_infection_timer_timeout() -> void:
+	EventBus.game_over.emit()
+	print("game over")

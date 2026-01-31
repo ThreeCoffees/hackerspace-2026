5ab6aee0e3f9c9ca35cc68548740b6892af8e76a
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-01-31 19:18:09 +0100

    fix running away bug

--- src/characters/npc/actions/grab_decoy_item_action.gd
+++ src/characters/npc/actions/grab_decoy_item_action.gd
@@ -4,9 +4,8 @@ extends ActionLeaf
 func tick(actor: Node, blackboard: Blackboard) -> int:
 	actor = actor as NPC
 
-	actor.set_run_away_point(blackboard.get_value("item_position"))
+	actor.nav_agent.target_position = blackboard.get_value("item_position")
+	blackboard.erase_value("item_position")
 
-	if not actor.nav_agent.is_navigation_finished():
-		return RUNNING
-	return SUCCESS
+	return RUNNING
 	

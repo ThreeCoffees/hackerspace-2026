e0a33a5fdea413796254ce74c46ac4b76d3ed8df
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-01-31 02:05:06 +0100

    throwing items

--- src/shared/shelves/shelf.gd
+++ src/shared/shelves/shelf.gd
@@ -2,9 +2,10 @@ class_name Shelf
 extends MeshInstance3D
 
 enum Item {
-	ToiletPaper,
-	Water,
-	Pasta,
+	TOILET_PAPER,
+	WATER,
+	PASTA,
+	NONE,
 }
 
 @export var interactable_area: Area3D
@@ -15,7 +16,7 @@ var is_picked: bool = false
 
 func _input(event: InputEvent) -> void:
 	if is_picked and event.is_action_pressed("interact_primary"):
-		ShoppingManager.interacted_with_shelf.emit(self)
+		EventBus.interacted_with_shelf.emit(self)
 
 
 func _on_static_body_3d_mouse_exited() -> void:

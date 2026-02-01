fd8cc00781a900a88152a0a989d0a25d75284aaa
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-02-01 11:29:26 +0100

    add throwable items

--- src/shared/items/item_thrower.gd
+++ src/shared/items/item_thrower.gd
@@ -10,9 +10,12 @@ func _ready() -> void:
 
 
 func _on_item_thrown(item: Shelf.Item) -> void:
-	print("item thrown")
+	print("item thrown ", item)
+	if item == Shelf.Item.NONE:
+		return
 	var item_scene = item_scenes[item]
-	var i = item_scene.instantiate() as RigidBody3D
+	var i = item_scene.instantiate()
+	print(i as RigidBody3D)
 	var throw_direction = Vector3.RIGHT.rotated(Vector3.UP, item_spawn.global_rotation.y)
 	i.position = item_spawn.global_position
 	i.apply_central_impulse(throw_direction * throw_strength)

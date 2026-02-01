6549891f6cada145ef281219891c4f92fe9fe716
ThreeCoffes <leonard.madzielewski@gmail.com>
2026-02-01 08:24:56 +0100

    start level on start btn pressed

---
+++ src/ui/scenes/main_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+@export_file_path() var level_path: String
+
+
+func _on_options_button_pressed() -> void:
+	pass
+
+func _on_start_button_pressed() -> void:
+	print("switching levels")
+	get_tree().change_scene_to_file(level_path)
+

916e5cb74740fc13ef368e0493b37070a51bf424
Andrzej Szreder <andriej2301@gmail.com>
2026-02-01 10:24:04 +0100

    feat: Animations for NPC

--- src/shared/animation/animation_component.gd
+++ src/shared/animation/animation_component.gd
@@ -5,8 +5,13 @@ extends Node
 @export var animation: AnimationPlayer
 
 
+func _ready():
+	player = get_parent() as CharacterBody3D
+	animation = player.find_child("AnimationPlayer", true, false) as AnimationPlayer
+
+
 func _physics_process(_delta: float) -> void:
 	if not player.velocity.is_zero_approx():
 		animation.play("Run")
 	else:
-		animation.pause()
+		animation.play("Idle")
